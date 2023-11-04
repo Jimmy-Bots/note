e7321dc00235af39fb41bf6f4ff5d528b1fa8b0d
Jimmy <2489219080@qq.com>
2023-11-04 23:03:08 +0800

    Deployed b9fe680 with MkDocs version: 1.5.3

--- assets/javascripts/decrypt-contents.js
+++ assets/javascripts/decrypt-contents.js
@@ -156,7 +156,7 @@ function decrypt_action(password_input, encrypted_content, decrypted_content) {
         // create HTML element for the inform message
         var decrypt_msg = document.createElement('p');
         decrypt_msg.setAttribute('id', 'mkdocs-decrypt-msg');
-        var node = document.createTextNode('ÃÜÂë´íÎó');
+        var node = document.createTextNode('å¯†ç é”™è¯¯');
         decrypt_msg.appendChild(node);
         var mkdocs_decrypt_msg = document.getElementById('mkdocs-decrypt-msg');
         // clear all previous failure messages
